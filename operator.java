ab0331d35a7537619c44490ae3e5c30d72088b55
Mohammed Arbaz Ali <42763403+Arbax96@users.noreply.github.com>
2018-08-30 20:54:32 +0530

    Add files via upload
    operator program in java like ++ += << >> ect

---
+++ operator.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class operator
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		public class Program
+{
+	public static void main(String[] args) {
+		int a=2;
+		int b=2;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		c=a-b;
+		System.out.println(c);
+		c=a*b;
+		System.out.println(c);
+		c=b/a;
+		System.out.println(c);
+		c=b%a;
+		System.out.println(c);
+		 c=b++;
+		System.out.println(c);
+		c=b--;
+		System.out.println(c);
+		c=a&b;
+		System.out.println(c);
+		c=a|b;
+		System.out.println(c);
+		c=a^b;
+		System.out.println(c);
+		c=~a;
+		System.out.println(c);
+	    c=a<<2;
+		System.out.println(c);
+		c=a>>2;
+		System.out.println(c);
+		c=a>>>2;
+		System.out.println(c);
+		/*c=a&&b;
+		System.out.println(c);
+		c=a||b;
+		System.out.println(c);
+		c=!(a&&b);
+		System.out.println(c);*/
+		
+
+
+	}
+}
