908c313fc8e13765cfde0a95bd0b30d1a1af16ab
Mohammed Arbaz Ali <42763403+Arbax96@users.noreply.github.com>
2018-09-10 21:00:55 +0530

    Add files via upload

---
+++ hwlloworld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args)
+   {
+     int n;
+     System.out.print("enter N");
+     Scanner s=new Scanner(System.in);
+     n=s.nextInt();
+     for (int i = 0; i < n; i++)
+      {
+			System.out.println("Hello World");
+      }
+  }
+}
